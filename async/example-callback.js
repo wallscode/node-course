26b82a411887cb8df93afc729f74871983f4c5f4
Jamie Walls <jwallsuva@hotmail.com>
2016-03-05 07:11:28 -0500

    Added example-callback.js

--- async/example-callback.js
+++ async/example-callback.js
@@ -5,5 +5,10 @@ request({
     url:url,
     json:true
 }, function(error, response, body) {
-    
-})
+    if (error) {
+        console.log('Unable to fetch weather.');
+    } else {
+        //console.log(JSON.stringify(body,null, 4));
+        console.log('It\'s ' + body.main.temp + ' in ' + body.name);
+    }
+});
